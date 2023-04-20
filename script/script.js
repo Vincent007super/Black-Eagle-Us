ee28fa711f4528d5147f9fbc357b9a8eaf3a8a92
Vincent007super <vrhoogenraat11@gmail.com>
2023-04-20 14:26:21 +0200

    Contact page style finished
    including javascript and css

--- script/script.js
+++ script/script.js
@@ -21,3 +21,13 @@ function ds_hide(){
     window.document.images[2].src='../media/ds.jpg';
     document.getElementById('discord').style.opacity = "1"
 }
+
+function yt_show(){
+  window.document.images[1].src='../media/yt(tr).png';
+  document.getElementById('youtube').style.opacity = "0.6"
+}
+
+function yt_hide(){
+  window.document.images[1].src='../media/yt.png';
+  document.getElementById('youtube').style.opacity = "1";
+}
